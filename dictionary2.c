95bd325a3b3fd44928157f9b21da7cab7b9ace4b
Hadar Asmaa <70861727+H-Asmaa@users.noreply.github.com>
2023-06-23 03:57:39 +0100

    Update dictionary2.c

--- dictionary2.c
+++ dictionary2.c
@@ -57,3 +57,32 @@ void subStack(stack_t **stack, unsigned int line_number)
     (*stack)->next = NULL;
     free(temp);
 }
+/**
+ * divStack - check the code
+ * @stack: first variable
+ * @line_number: Second variable
+ * Return: void.
+ */
+void divStack(stack_t **stack, unsigned int line_number)
+{
+	int num1, num2;
+	stack_t *tmp;
+
+	if (*stack == NULL || (*stack)->prev == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	num1 = (*stack)->n;
+	num2 = (*stack)->prev->n;
+	(*stack)->prev->n = num1 / num2;
+	tmp = *stack;
+	*stack = (*stack)->prev;
+	(*stack)->next = NULL;
+	free(tmp);
+}
